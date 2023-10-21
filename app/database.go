574e4fe785325f77ec806730620e243f67c33315
Fatwah Firmansyah <fatwah091@gmail.com>
2023-10-21 13:04:14 +0700

    adding migrate package and create migrations for category table

--- app/database.go
+++ app/database.go
@@ -8,7 +8,7 @@ import (
 )
 
 func NewDb() *sql.DB {
-	db, err := sql.Open("mysql", "root@tcp(localhost:3306)/go_learn")
+	db, err := sql.Open("mysql", "root@tcp(localhost:3306)/golearn_migration")
 	helpers.PanicIfError(err)
 
 	db.SetMaxIdleConns(5)
@@ -17,4 +17,23 @@ func NewDb() *sql.DB {
 	db.SetConnMaxLifetime(60 * time.Minute)
 
 	return db
+
+	//UP
+	//migrate -database "mysql://root@tcp(localhost:3306)/golearn_migration" -path db/migrations up
+
+	//DOWN
+	//migrate -database "mysql://root@tcp(localhost:3306)/golearn_migration" -path db/migrations down
+
+	//UP SPECIFIC VERSION
+	//migrate -database "mysql://root@tcp(localhost:3306)/golearn_migration" -path db/migrations UP (1..N)
+
+	//DOWN SPECIFIC VERSION
+	//migrate -database "mysql://root@tcp(localhost:3306)/golearn_migration" -path db/migrations DOWN (1..N)
+
+	//CHECK VERSION
+	//migrate -database "mysql://root@tcp(localhost:3306)/golearn_migration" -path db/migrations version
+
+	//FORCE MIGRATION
+	//migrate -database "mysql://root@tcp(localhost:3306)/golearn_migration" -path db/migrations force 000001(version)
+
 }
